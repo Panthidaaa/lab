2cf226b476cbbeedc2e18f2920ee1e99bea2cd64
Panthidaaa <jumbo03947@gmail.com>
2025-08-21 09:41:22 +0700

    Add files via upload

---
+++ labsql.sql
@@ -0,0 +1,25 @@
+﻿-- แสดงชื่อประเภทสินค้า ชื่อสินค้า 
+select CategoryName, ProductName, UnitPrice
+from Products, Categories
+where Products.CategoryID = Categories.CategoryID
+
+select categoryName,ProductName,UnitPrice
+from Products JOIN Categories
+on Products.CategoryID = Categories.CategoryID
+
+select  CompanyName, OrderID
+From Orders, Shippers
+Where Shippers.ShipperID = Orders.Shipvia
+
+select  CompanyName, OrderID
+From Orders JOIN Shippers
+on Shippers.ShipperID = Orders.Shipvia
+
+--ต้องการรหัสสินค้า ชื่อสินค้า บริษัทผู้จำหน่าย ประเทศ
+select p.ProductID,ProductName,s.CompanyName,s.Country
+from Products p JOIN suppliers s on p.SupplierID = s.SupplierID
+
+select p.ProductID,p.ProductName,s.CompanyName, s.Country
+from Products p JOIN suppliers s on p.SupplierID = s.SupplierID 
+
+select CompanyName, OrderID
